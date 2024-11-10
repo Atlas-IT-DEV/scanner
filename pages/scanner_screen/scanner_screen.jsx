465aa30e927a73c5abc13948149a13343ccd7d0c
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-11-11 00:06:29 +0300

    added logic model

--- pages/scanner_screen/scanner_screen.jsx
+++ pages/scanner_screen/scanner_screen.jsx
@@ -1,5 +1,7 @@
 import {
+  Image,
   ImageBackground,
+  Modal,
   SafeAreaView,
   ScrollView,
   StyleSheet,
@@ -19,7 +21,6 @@ const ScannerScreen = ({ route }) => {
   const [products, setProducts] = useState([]);
   const [uniqCategories, setUniqCategories] = useState([]);
   const { pageStore } = useStores();
-
   useEffect(() => {
     const getAllProducts = async () => {
       const response = await fetch(
@@ -28,7 +29,7 @@ const ScannerScreen = ({ route }) => {
           method: "GET",
           headers: {
             accept: "application/json",
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${pageStore.token}`,
           },
         }
       );
@@ -42,7 +43,79 @@ const ScannerScreen = ({ route }) => {
     };
     getAllProducts();
   }, []);
-  console.log(uniqCategories);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [photo, setPhoto] = useState(null);
+  const [rotated, setRotated] = useState(false);
+  const [result, setResult] = useState("");
+  const [status, setStatus] = useState(0);
+  const [itemId, setItemId] = useState(1);
+
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    if (photo) {
+      performInference();
+    }
+  }, [photo, itemId]);
+
+  const openImagePicker = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert("Ошибка", "Необходимо разрешение на доступ к галерее.");
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [3, 4],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setPhoto(result.assets[0].uri);
+    }
+  };
+
+  const performInference = async () => {
+    setStatus(1);
+    const formData = new FormData();
+    formData.append("input", {
+      uri: photo,
+      type: "image/jpeg",
+      name: "man.jpg",
+    });
+    formData.append("product_id", `${itemId}`);
+
+    try {
+      const response = await fetch("https://orion-lab.tech:8010/inference/", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${pageStore.token}`,
+        },
+        body: formData,
+      });
+      const arrayBuffer = await response.arrayBuffer();
+      const base64Image = `data:image/png;base64,${Buffer.from(
+        arrayBuffer
+      ).toString("base64")}`;
+      setResult(base64Image);
+      setStatus(2);
+      console.log(base64Image);
+    } catch (error) {
+      setStatus(3);
+      console.error("Ошибка при отправке изображения:", error);
+    }
+  };
+
+  const filteredCards = category
+    ? products.filter((card) => card.category === category)
+    : products;
   return (
     <SafeAreaView style={styles.container}>
       <Header />
@@ -53,9 +126,14 @@ const ScannerScreen = ({ route }) => {
       >
         <ScrollView style={{ marginTop: 80 }}>
           <View style={{ marginTop: 20 }}>
-            <BotMessage />
+            {!modalVisible ? <BotMessage /> : null}
           </View>
-          <SvgXml xml={man} style={styles.man} />
+          <SvgXml
+            xml={man}
+            style={styles.man}
+            width={modalVisible ? 120 : 160}
+            height={modalVisible ? 263 : 340}
+          />
         </ScrollView>
         <ScrollView
           horizontal
@@ -70,23 +148,103 @@ const ScannerScreen = ({ route }) => {
           }}
         >
           {uniqCategories.map((item) => {
-            console.log(item);
             return (
-              <TouchableOpacity style={styles.categoryButton}>
-                <Text style={styles.categoryText}>
-                  {item == "upperbody"
-                    ? "Верхняя одежда"
-                    : item == "dress"
-                    ? "Платье"
-                    : null}
-                </Text>
-              </TouchableOpacity>
+              <>
+                {!modalVisible ? (
+                  <TouchableOpacity
+                    style={styles.categoryButton}
+                    onPress={() => {
+                      setCategory(item);
+                      setModalVisible(true);
+                    }}
+                  >
+                    <Text style={styles.categoryText}>
+                      {item == "upperbody"
+                        ? "Верхняя одежда"
+                        : item == "dress"
+                        ? "Платье"
+                        : null}
+                    </Text>
+                  </TouchableOpacity>
+                ) : null}
+              </>
             );
           })}
         </ScrollView>
       </ImageBackground>
 
-      <BottomMenu />
+      {!modalVisible ? <BottomMenu /> : null}
+      <Modal visible={modalVisible} animationType="slide" transparent={true}>
+        <View
+          style={{
+            position: "absolute",
+            left: 0,
+            bottom: 0,
+            width: "100%",
+            height: 300,
+            padding: 25,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => setModalVisible(false)}
+            style={{
+              alignItems: "center",
+              backgroundColor: "white",
+              padding: 10,
+              borderRadius: 8,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: "RalewayMedium",
+                fontSize: 12,
+                color: "black",
+              }}
+            >
+              {category == "upperbody"
+                ? "Верхняя одежда"
+                : category == "dress"
+                ? "Платье"
+                : null}
+            </Text>
+          </TouchableOpacity>
+          <ScrollView
+            style={{
+              marginTop: 10,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                flexWrap: "wrap",
+                columnGap: 24,
+                rowGap: 10,
+              }}
+            >
+              {filteredCards.map((item, index) => {
+                console.log(index)
+                return (
+                  <TouchableOpacity
+                    style={{
+                      width: 105,
+                      height: 97,
+                      backgroundColor: "white",
+                      borderRadius: 7,
+                    }}
+                  >
+                    <Image
+                      source={{
+                        uri: `https://orion-lab.tech:8010${item.image.url}`,
+                      }}
+                      style={{ width: 66, height: 66, resizeMode: "cover" }}
+                    />
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+          </ScrollView>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
